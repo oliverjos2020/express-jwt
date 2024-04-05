16cd9d7643d89c71b824493c7bebafa79ee07b52
Ezekiel Afolabi <ezekialafolabi11@gmail.com>
2024-04-05 17:02:34 +0100

    Created the edit and update module

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const { validationResult } = require("express-validator");
 
 
 const getUsers = async (req, res) => {
@@ -11,6 +12,82 @@ const getUsers = async (req, res) => {
   }
 };
 
+const editUsers = async (req, res) => {
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+     return res
+       .status(400)
+       .json({ responseCode: 400, responseMessage: errors.array() });
+   }
+  const { id } = req.body;
+  const userdb = await User.findOne({ _id: id });
+  if (userdb) {
+    return res.status(200).json({
+      responseMessage: "Record Found",
+      responseCode: 200,
+      data: {name: userdb.name, email: userdb.email, phone: userdb.phone, id:id}
+    });
+  } else {
+    return res.status(401).send({
+      responseMessage: "Record not found",
+      responseCode: 401,
+    });
+  }
+  console.log(userdb);
+
+}
+
+const updateUsers = async (req, res) => { 
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({ responseCode: 400, responseMessage: errors.array() });
+  }
+
+  
+  
+  try {
+    const { name, phone, email, id } = req.body;
+
+    const userData = { name, phone, email };
+    const updatedUser = await User.findByIdAndUpdate(id, userData, {
+      new: true,
+    });
+    if (!updatedUser) {
+      return res
+        .status(404)
+        .json({ responseCode: 404, responseMessage: "User not found" });
+    }
+     return res
+       .status(200)
+       .json({
+         responseCode: 200,
+         responseMessage: "User updated successfully",
+         user: updatedUser,
+       });
+  } catch (error) {
+    // Handle any errors that occur during the update process
+    console.error("Error updating user:", error);
+    if (error.name === 'MongoError' && error.code === 11000) {
+      return res.status(400).json({ responseCode: 400, responseMessage: "Duplicate email address" });
+    }
+    
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+      return res.status(400).json({ responseCode: 400, responseMessage: "Invalid user ID" });
+    }
+
+    // Handle database connection errors
+    console.error("Database connection error:", error);
+    return res.status(500).json({ responseCode: 500, responseMessage: "Database connection error" });
+  
+  }
+  
+
+}
+
 module.exports = {
-  getUsers
+  getUsers,
+  editUsers,
+  updateUsers
 };
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var router = express.Router();
-const { getUsers } = require("../controllers/userController");
+const { getUsers, editUsers, updateUsers } = require("../controllers/userController");
+const { body } = require("express-validator");
+const { requireAuth, checkUser } = require("../middleware/authMiddleware");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get("/getusers", getUsers);
+router.get("/getusers", requireAuth, getUsers);
+// router.get("/editusers", editUsers);
+router.post(
+  "/editusers",
+  requireAuth, [body("id").notEmpty().withMessage("Id is required")],
+  editUsers
+);
+
+router.post(
+  "/update",
+  [
+    body("name").notEmpty().withMessage("Name field is required"),
+    body("email").isEmail().withMessage("Invalid email address"),
+    body("phone").isMobilePhone().withMessage("Invalid phone number")
+  ],
+  updateUsers
+);
 
 module.exports = router;
